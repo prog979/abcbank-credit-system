1cd5241d6462c5c716f69fa149b891c1dc9044c0
Chekalov <prog979@yandex.ru>
2021-11-01 17:14:07 +0300

    SQL files are aligned with entities classes

--- SQL/tables.sql
+++ SQL/tables.sql
@@ -1,40 +1,41 @@
-CREATE TABLE IF NOT EXISTS clients
+CREATE TABLE IF NOT EXISTS CLIENTS
 (
-    id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    secname VARCHAR (255),
-    name VARCHAR (255),
-    patronymic VARCHAR (255),
-    pasport_number VARCHAR (255),
-    email VARCHAR (255),
-    phone_number VARCHAR (255)
-    )
+    id             CHAR(36) NOT NULL PRIMARY KEY,
+    secname        VARCHAR(255),
+    name           VARCHAR(255),
+    patronymic     VARCHAR(255),
+    pasport_number VARCHAR(255),
+    email          VARCHAR(255),
+    phone_number   VARCHAR(255)
+);
 
-CREATE TABLE IF NOT EXISTS credits
+CREATE TABLE IF NOT EXISTS CREDITS
 (
-    id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    credit_name VARCHAR (255),
-    credit_limit BIGINT NOT NULL,
-    percent integer NOT NULL)
+    id           CHAR(36) NOT NULL PRIMARY KEY,
+    credit_name  VARCHAR(255),
+    credit_limit BIGINT   NOT NULL,
+    percent      integer  NOT NULL
+);
 
-CREATE TABLE IF NOT EXISTS credit_offers
+CREATE TABLE IF NOT EXISTS CREDIT_OFFERS
 (
-    id               BIGINT       NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id               CHAR(36)     NOT NULL PRIMARY KEY,
     credit_sum       BIGINT,
     months_of_credit BIGINT,
     client_id        VARCHAR(255) NOT NULL,
     credit_id        VARCHAR(255) NOT NULL,
-    FOREIGN KEY (client_id) REFERENCES client (id) ON DELETE CASCADE,
-    FOREIGN KEY (credit_id) REFERENCES credit (id) ON DELETE CASCADE
-)
+    FOREIGN KEY (client_id) REFERENCES CLIENTS (id) ON DELETE CASCADE,
+    FOREIGN KEY (credit_id) REFERENCES CREDITS (id) ON DELETE CASCADE
+);
 
 CREATE TABLE IF NOT EXISTS payment_schedules
 (
-    id              BIGINT       NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id              BIGINT       NOT NULL PRIMARY KEY,
     credit_body     BIGINT,
     credit_percents BIGINT,
     date            Date,
     payment_rest    BIGINT,
     payment_sum     BIGINT,
     credit_offer_id VARCHAR(255) not null,
-    FOREIGN KEY (credit_offer_id) REFERENCES credit_offer (id)
+    FOREIGN KEY (credit_offer_id) REFERENCES CREDIT_OFFERS (id)
 )
